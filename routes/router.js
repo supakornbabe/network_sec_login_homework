30de24d6025215501cfc77d9747e9585689df249
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-04-01 02:02:32 +0700

    add back button

--- routes/router.js
+++ routes/router.js
@@ -40,7 +40,7 @@ router.post('/', function (req, res, next) {
     } else if (req.body.logusername && req.body.logpassword) {
         User.authenticate(req.body.logusername, req.body.logpassword, function (error, user) {
             if (error || !user) {
-                var err = new Error('Wrong email or password.');
+                var err = new Error('Wrong email or password.' + '<br><a type="button" href="/">BACK</a>');
                 err.status = 401;
                 return next(err);
             } else {
